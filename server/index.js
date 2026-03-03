fd2fa9fa79c4ed9ee5a85778ede978b607ac68e3
Ronan Hilditch <ronan@gmail.com>
2026-03-03 18:36:38 -0500

    Fixed Basic Error

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/Clicker", express.static(path.join(__dirname, '../public/Clicker')));
-app.use("/KMAIL", express.static(path.join(__dirname, '../public/EMAIL')));
-app.use("/2048", express.static(path.join(__dirname, '../public/2048')));
-app.use("/Login", express.static(path.join(__dirname, '../public/Login')));
+app.use("/Clicker", express.static(path.join(__dirname, '../public/Clicker/Clicker.html')));
+app.use("/KMAIL", express.static(path.join(__dirname, '../public/EMAIL/index.html')));
+app.use("/2048", express.static(path.join(__dirname, '../public/2048/index.html')));
+app.use("/Login", express.static(path.join(__dirname, '../public/Login/page.html')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
