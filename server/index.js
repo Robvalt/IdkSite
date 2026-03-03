3dc0e79dc46df8db6ec7615cea14e1df1b5ffed9
Ronan Hilditch <ronan@gmail.com>
2026-03-03 17:12:26 -0500

    Fixed Serving Error

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public/html')));
+app.use(express.static(path.join(__dirname, '../public/html')));
 
 
 app.listen(port, () => {
--- server/test.js
+++ server/test.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public/html')));
+app.use(express.static(path.join(__dirname, '../public/html')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/html/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
