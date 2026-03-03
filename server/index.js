e711fe70cae38d9691d4b4130834c1d0fac906e2
Ronan Hilditch <ronan@gmail.com>
2026-03-03 18:44:02 -0500

    Fixed Serving Path Error

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
+const e = require('express');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/Clicker", express.static(path.join(__dirname, '../public/Clicker/Clicker.html')));
-app.use("/KMAIL", express.static(path.join(__dirname, '../public/EMAIL/index.html')));
-app.use("/2048", express.static(path.join(__dirname, '../public/2048/index.html')));
-app.use("/Login", express.static(path.join(__dirname, '../public/Login/page.html')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });
+
+app.use("/Clicker", express.static(path.join(__dirname, '../public/html/Clicker/Clicker.html')));
+app.use("/KMAIL", express.static(path.join(__dirname, '../public/html/EMAIL/index.html')));
+app.use("/2048", express.static(path.join(__dirname, '../public/html/2048/index.html')));
+app.use("/Login", express.static(path.join(__dirname, '../public/html/Login/page.html')));
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
