e29a8c097a64f18ade80fc5b686a719968228e33
Blink <111193038+Robvalt@users.noreply.github.com>
2026-03-04 08:35:12 -0500

    Enhance server with Clicker, Bakery routes and logging
    Added static file serving for Clicker and Bakery sections, and implemented a logger middleware to log requests.

--- server/index.js
+++ server/index.js
@@ -7,13 +7,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
+// CLICKER
 app.use("/Clicker", express.static(path.join(__dirname, '../public/html/Clicker')));
+app.use("/Clicker/Shop", express.static(path.join(__dirname, '../public/html/Clicker/Shop')));
+
 app.use("/KMAIL", express.static(path.join(__dirname, '../public/html/EMAIL')));
 app.use("/2048", express.static(path.join(__dirname, '../public/html/2048')));
 app.use("/Login", express.static(path.join(__dirname, '../public/html/Login')));
 app.use("/Gaming", express.static(path.join(__dirname, '../public/html/Gaming')));
+
+// BAKERY URLS
 app.use("/Bakery", express.static(path.join(__dirname, '../public/html/bakery')));
+app.use("/Bakery/Images", express.static(path.join(__dirname, '../public/html/bakery')));
+
+// LOGGER
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next(); // Pass control to the next handler
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log('Server is running on port ${port}');
+    console.log('Waiting For Requests...');
 });
