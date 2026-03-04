61e30400f8f7d9f695fc7f1b62222492928ad2b5
Blink <111193038+Robvalt@users.noreply.github.com>
2026-03-04 09:33:09 -0500

    Add logging to the root route handler

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
 });
 
 // CLICKER
